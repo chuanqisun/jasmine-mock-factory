0dc734202891769944351e2e44399791a6a5a0ab
Chuanqi Sun <chusun@microsoft.com>
2017-06-15 16:37:55 -0700

    clean up code base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({})
 export class AppModule { }
